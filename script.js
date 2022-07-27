85de30576b9b831486a501b6ec9660f6363269e0
Sam Foit <smfoit4311@gmail.com>
2022-07-27 15:30:29 -0500

    Added Sketching

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ for (let j = 0; j < 16; j++){
     for (let i = 0; i < 16; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
+        div.addEventListener('mouseenter', function() {
+            this.style.backgroundColor = "black";
+        })
         container.appendChild(div);
     }
 }
+
+const pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach()
